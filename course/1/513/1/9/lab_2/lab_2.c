50258d86277b849d4c8587fdc51e2af18ac2fa7d
Alex Talker <alextalker@openmailbox.org>
2013-11-05 19:22:02 +0400

    bug fix

--- course/1/513/1/9/lab_2/lab_2.c
+++ course/1/513/1/9/lab_2/lab_2.c
@@ -3,11 +3,10 @@
 
 int main(void)
 {
-    //TODO: сделать расчеты на бумаге для проверки
     double a,b,z1,z2;
     scanf("%lf %lf", &a, &b);
     z1 = (pow((cos(a) - cos(b)), 2))-(pow(( sin(a) - sin(b)), 2));
-    z2 = (sin(a+b))*(sin(a-b));
+    z2 = -4*(pow(sin((a-b)/2), 2.0))*cos(a+b);
     printf("z1 = %lf\nz2 = %lf\n", z1, z2);
     return 0;
 }
